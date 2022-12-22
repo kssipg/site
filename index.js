de38e38e3151617b54600258d70c3901923e64de
kssipg <kutiasmr@gmail.com>
2022-12-22 07:12:40 +0000

    Hello

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-import express from "express";
-import bodyParser from "body-parser";
-const app = express();
-app.use (bodyParser.urlencode({extended: true}));
+import express from 'express'
+import bodyParser from 'body-parser'
+const app = express()
+const port = process.env.PORT || 8080
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 app.get('/', (req, res) =>
 {
-    res.render ('index.ejs');
-})
-app.post('/', (req, res) =>
+      res.render('index.ejs')
+});
 
-const port = process.env.PORT || 8080
-app.listen(port, () => console.log(`listen port: ${port}`))
+app.post('/', (req, res) =>
+{
+    const username = req.body.username;
+    res.render('hello.ejs', {username: username});
+});
+
+app.listen(port, () => {
+      console.log(`listen port ${port}`)
+})
