82e1487c83e17323995cad1f064d3342fb9f38f5
kssipg <kutiasmr@gmail.com>
2022-12-22 06:56:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+const app = express();
+app.use (bodyParser.urlencode({extended: true}));
+app.get('/', (req, res) =>
+{
+    res.render ('index.ejs');
+})
+app.post('/', (req, res) =>
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`listen port: ${port}`))
